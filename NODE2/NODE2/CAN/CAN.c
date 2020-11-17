fc4abfea5a75aa84d8808bab1e5c2b910bf85e81
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 09:29:54 +0100

    Update CAN.c

--- NODE2/NODE2/CAN/CAN.c
+++ NODE2/NODE2/CAN/CAN.c
@@ -1,3 +1,13 @@
+/**
+ * @file CAN.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing top level drivers for MCU based CAN bus communication.
+ * Recieved messages is put into a rolling, FIFO register of CAN messages.
+ * Messages are also handled at recieve time, to update relevant global datastructures.
+ * See msg_handler.h
+ */
+
 
 #include "CAN.h"
 #include "sam.h"
@@ -6,7 +16,7 @@
 #include "can_interrupt.h"
 #include "../Utility/Utility.h"
 /*
-//MCK ==16MHz eller 84???
+//MCK == 84MHz
 //CAN BR:
 //b24 == sampling mode 0 = once, 1 = three times.
 //b16-22 == BRP Baud rate prescaler. t_can_system_clock == TQ == (BRP+1)/MCK. MCK = 8MHz BRP == 32. TQ = 1us = 83
@@ -24,13 +34,18 @@
 #define CAN_PHASE2 (6 << 0)
 #define can_br (CAN_BRP | CAN_SMP | CAN_PROPAG | CAN_SJW | CAN_PHASE1 | CAN_PHASE2)
 
-volatile CAN_MESSAGE CAN_INPUT_BUFFER[8];
-volatile uint8_t CAN_BUFFER_CTR = 0;
-volatile uint8_t CAN_first_rx = 0;
-volatile uint8_t valid_messages = 0;
+volatile CAN_MESSAGE CAN_INPUT_BUFFER[8]; ///< @brief Rolling CAN input message buffer 
+volatile uint8_t CAN_BUFFER_CTR = 0; ///< @brief CAN input message buffer counter
+volatile uint8_t CAN_first_rx = 0; ///< @brief First valid CAN_input message flag
+volatile uint8_t valid_messages = 0; ///< @brief Number of valid CAN messages in input buffer
 
 static volatile int flag = 0;
-
+/**
+ * @brief Read data from input buffer
+ *
+ * @param  RX_packet: pointer to data destination
+ * @retval int8_t: fail/success
+ */ 
 int8_t CAN_read(CAN_MESSAGE* RX_packet)
 {
 	
@@ -61,14 +76,22 @@ int CAN_interrupt()
 	return 0;
 }
 
+/**
+ * @brief Initialization function for the top layer CAN interface.
+ * Configures relevant peripherals and goes through initialization-
+ * sequence
+ *
+ * @param  mode: MCP2515 mode
+ * @retval none
+ */ 
 void CAN_init(uint8_t mode)
 {
-	for(uint8_t i = 0; i < 8; i++)
+	for(uint8_t i = 0; i < 8; i++) //Initialize all CAN input buffer IDs to zero
 	{
 		CAN_INPUT_BUFFER[i].ID = 0;
 	}
 	
-	can_init_def_tx_rx_mb(can_br);
+	can_init_def_tx_rx_mb(can_br); 
 	
 	TC2_init(750000); //Timer for ready-pinging of NODE1
 	Timer_start(TIMER2);
