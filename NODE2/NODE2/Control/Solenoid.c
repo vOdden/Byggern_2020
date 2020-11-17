aa3aa9661261fea6643a982bee8bdb13772e3b31
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 10:37:59 +0100

    Update Solenoid.c

--- NODE2/NODE2/Control/Solenoid.c
+++ NODE2/NODE2/Control/Solenoid.c
@@ -1,3 +1,10 @@
+/**
+ * @file Solenoid.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing drivers for solenoid
+ *
+ */
 
 #include "Solenoid.h"
 #include <stdio.h>
@@ -7,7 +14,7 @@
 
 void Solenoid_init(void)
 {
-	//use PB26 on board => Pin 22
+	//use PB26 on board => Pin 22 on shield
 	PIOB->PIO_PER |= PIO_PER_P26; //enable PIO controll on pin 22
 	PIOB->PIO_OER |= PIO_OER_P26; //enable output on pin 22
 	PIOB->PIO_CODR |= PIO_CODR_P26; //set the output to 0
@@ -16,21 +23,14 @@ void Solenoid_init(void)
 	TC1_init(SHOT_DURATION * 1000);
 } 
 
-void Solenoid_shoot(void)
+void Solenoid_shoot(void)//start procedure for shooting solenoid
 {
 	set_pin(SOLENOID_PORT, SOLENOID_PIN);
 	Timer_start(TIMER1);
 }
 
-
-/*			TC0 TC1 TC2 TC3	TC4 TC5 TC6	 				MCU -> NVIC
-//  TC1		CH0 CH1 CH2									PERIPHERAL
-//  TC2                 CH0 CH1 CH2						PERIPHERAL
-//  TC3                               CH0
-
-*/
 #include "../Control/Motor.h"
-void TC3_Handler(void)
+void TC3_Handler(void)// stops solenoid and stops timer. 
 {
 	uint32_t status = REG_TC1_SR0;//read status register - this clears interrupt flags
 	clear_pin(SOLENOID_PORT, SOLENOID_PIN);
