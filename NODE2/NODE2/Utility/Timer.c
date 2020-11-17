31eb5d6790255fdb427a840e28c73c929c7bce40
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 11:01:21 +0100

    Update Timer.c

--- NODE2/NODE2/Utility/Timer.c
+++ NODE2/NODE2/Utility/Timer.c
@@ -1,9 +1,20 @@
+/**
+ * @file Timer.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing drivers for timer
+ */
 #include "sam.h"
 #include <stdio.h>
 #include "Timer.h"
 
-
-void TC0_init(uint32_t periode) //Periode i us
+/**
+ * @brief Initialization function for timer 0
+ *
+ * @param  period
+ * @retval none
+ */ 
+void TC0_init(uint32_t periode) //Period in us
 {
 	NVIC_EnableIRQ(TC0_IRQn); // Enable TC0 IRQ
 	REG_PMC_PCER0 |= PMC_PCER0_PID27; // Enable TC0 clock in PMC
@@ -16,12 +27,19 @@ void TC0_init(uint32_t periode) //Periode i us
 	//enable tc clock
 	REG_TC0_CCR0 |= TC_CCR_CLKEN;
 
-	uint32_t tick = periode / 0.095;
-
+	uint32_t tick = periode / 0.095;// every tick == 1/f second. => time = number of ticks / f => number of ticks = time * f
+	// wants 2 us. Timer goes on a period of 0.095us
+	// Have to wait 2/0.0.95 tick ~= 5
 	REG_TC0_RC0 = tick;
 }
 
-void TC1_init(uint32_t periode) //Periode i us
+/**
+ * @brief Initialization function for timer 1
+ *
+ * @param  period
+ * @retval none
+ */
+void TC1_init(uint32_t periode) //Period in us
 {
 	NVIC_EnableIRQ(TC3_IRQn); // Enable TC3 IRQ
 	REG_PMC_PCER0 |= PMC_PCER0_PID30; // Enable TC3 clock in PMC
@@ -34,13 +52,19 @@ void TC1_init(uint32_t periode) //Periode i us
 	//enable tc clock
 	REG_TC1_CCR0 |= TC_CCR_CLKEN;
 
-	uint32_t tick = periode / 0.095; // hvert tick == 1/f sekund. Dvs => tid = antall tick / f => antall tick = tid * f
-	// Vil ha 2 us. Timeren går i en periode på 0.095us
-	// Da må vi vente i 2/0.0.95 tick ~= 5
+	uint32_t tick = periode / 0.095; // every tick == 1/f second. => time = number of ticks / f => number of ticks = time * f
+	// wants 2 us. Timer goes on a period of 0.095us
+	// Have to wait 2/0.0.95 tick ~= 5
 	REG_TC1_RC0 = tick;
 }
 
-void TC2_init(uint32_t periode) //Periode i us
+/**
+ * @brief Initialization function for timer 2
+ *
+ * @param  period
+ * @retval none
+ */
+void TC2_init(uint32_t periode) //Period in us
 {
 	NVIC_EnableIRQ(TC6_IRQn); // Enable TC3 IRQ
 	REG_PMC_PCER1 |= PMC_PCER1_PID33; // Enable TC3 clock in PMC
@@ -53,12 +77,18 @@ void TC2_init(uint32_t periode) //Periode i us
 	//enable tc clock
 	REG_TC2_CCR0 |= TC_CCR_CLKEN;
 
-	uint32_t tick = periode / 0.095; // hvert tick == 1/f sekund. Dvs => tid = antall tick / f => antall tick = tid * f
-	// Vil ha 2 us. Timeren går i en periode på 0.095us
-	// Da må vi vente i 2/0.0.95 tick ~= 5
+	uint32_t tick = periode / 0.095; // every tick == 1/f second. => time = number of ticks / f => number of ticks = time * f
+	// wants 2 us. Timer goes on a period of 0.095us
+	// Have to wait 2/0.0.95 tick ~= 5
 	REG_TC2_RC0 = tick;
 }
 
+/**
+ * @brief Start timer
+ *
+ * @param  timer
+ * @retval none
+ */ 
 void Timer_start(int timer)
 {
 	switch(timer)
@@ -80,6 +110,13 @@ void Timer_start(int timer)
 		
 	}
 }
+
+/**
+ * @brief stop timer
+ *
+ * @param  timer
+ * @retval none
+ */ 
 
 void Timer_stop(int timer)
 {
