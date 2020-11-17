c5bc01e5c5d12be63b4d88eaf6574d6cfb53da57
philisg <71253972+philisg@users.noreply.github.com>
2020-11-17 10:53:01 +0100

    Update PWM.c

--- NODE2/NODE2/Utility/PWM.c
+++ NODE2/NODE2/Utility/PWM.c
@@ -1,18 +1,31 @@
+/**
+ * @file PWM.c
+ * @author TTK4155 2020 Group 28
+ * @date 17 nov 2020
+ * @brief File containing drivers for PWM.
+ */
 #include "PWM.h"
 
+
+/**
+ * @brief Initialization function for PWM
+ *
+ * @param  none
+ * @retval none
+ */ 
 void PWM_init(void)
 {
-	PIOC->PIO_PDR |= PIO_PDR_P19;
-	PIOC->PIO_ABSR |= PIO_ABSR_P19; //PC19 = pin 44 on shield
-	PIOC->PIO_PDR |= PIO_PDR_P18;
-	PIOC->PIO_ABSR |= PIO_ABSR_P18; //PC18 = pin 45 on shield
+	PIOC->PIO_PDR |= PIO_PDR_P19; //Disable PIO control
+	PIOC->PIO_ABSR |= PIO_ABSR_P19; //PIO multiplexer set to B
+	PIOC->PIO_PDR |= PIO_PDR_P18; //Disable PIO control
+	PIOC->PIO_ABSR |= PIO_ABSR_P18; //PIO multiplexer set to B
 	
 	PWM->PWM_WPCR = PWM_WPCR_WPKEY(0x50574D)| PWM_WPCR_WPRG0 |PWM_WPCR_WPRG2|PWM_WPCR_WPRG3;//Enables writing to register group 0,2,3
 	
-	PWM->PWM_CLK = PWM_CLK_DIVA(28)| PWM_CLK_PREA(0); //PWM_CLK = MCK = 84MHz
+	PWM->PWM_CLK = PWM_CLK_DIVA(28)| PWM_CLK_PREA(0); //PWM_CLK = 3MHZ
 	PMC->PMC_PCER1 |= (1 << 4); //Enable PWM clock
 	REG_PWM_CMR5 = 0xB | 1 << 9; //CLKA, Polatity start high
-	REG_PWM_CPRD5 = 60000;
+	REG_PWM_CPRD5 = 60000; // set 
 	REG_PWM_CDTY5 = 4500; // Hver verdi tilsvarer 1us
 	REG_PWM_CMR6 = 0xB | 1 << 9; //CLKA, Polatity start high
 	REG_PWM_CPRD6 = 60000;
